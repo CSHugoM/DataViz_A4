4bca7a4e4213002f0057dbc441c3d92f0761b598
paulabeergorodicht <97967721+paulabeergorodicht@users.noreply.github.com>
2022-03-28 09:34:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,13 @@ raw_data, agg_data = load_data()
 # This command sets the page to full screen
 st.set_page_config(layout="wide")
 
+# Test title
+st.title('Gender Differences in the World')
+st.subheader("An overview to understand the context and evolution of women's health, employment and education levels")
+st.text(" ")
+st.text(" ")
+st.text(" ")
+
 # We compute the global mean to be displayed at the start
 global_means = compute_mean(agg_data, list_index = None, list_var = list_global_var)
 
@@ -25,6 +32,10 @@ icons_container = st.empty()
 
 st_icons(global_means, icons_container)
 
+#refresh button
+if st.button("Reset"):
+    raise st.experimental_rerun()
+    
 st_map(agg_data, list_global_var, icons_container, global_means)
 
 st_charts(agg_data)
