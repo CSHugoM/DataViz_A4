e9dedf7c6fddaca9699741197784b9724283ce9c
paulabeergorodicht <97967721+paulabeergorodicht@users.noreply.github.com>
2022-03-28 10:29:23 +0200

    Update charts.py

--- components/charts.py
+++ components/charts.py
@@ -10,10 +10,10 @@ def st_charts(data : pd.DataFrame):
     cols = st.columns([1,2])
     cols[0].write("#")
     cols[0].write("#")
-    cols[0].text("Variable 1")
+    cols[0].markdown("**Variable 1**")
     choosen_variable_1 = cols[0].selectbox('', tuple(list(data.columns)), key = "1")
     cols[0].write("#")
-    cols[0].text("Variable 2")
+    cols[0].markdown("**Variable 2**")
     choosen_variable_2 = cols[0].selectbox('', tuple(list(data.columns)), key = "2")
     cols[0].write("#")
     df = data[[choosen_variable_1,choosen_variable_2]]
@@ -23,7 +23,7 @@ def st_charts(data : pd.DataFrame):
     else:
         r =  scipy.stats.pearsonr(df[choosen_variable_1],df[choosen_variable_2])
     r_report = "Correlation: " + str(round(r[0],2))
-    cols[0].text(r_report)
+    cols[0].markdown(r_report)
     chart = px.scatter(data,x=choosen_variable_1, y = choosen_variable_2, trendline="ols")
     chart.update_traces(textposition='top center')
     chart.update_layout(height = 800, width = 800)
